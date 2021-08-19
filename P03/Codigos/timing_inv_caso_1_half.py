9a6a761bff8e576394cc232a7ac40d3c580b3a5c
danielandes <88337429+danielandes@users.noreply.github.com>
2021-08-18 20:38:18 -0400

    Add files via upload

---
+++ P03/Codigos/timing_inv_caso_1_half.py
@@ -0,0 +1,32 @@
+from numpy import zeros, half
+from numpy.linalg import inv
+from time import perf_counter
+import matplotlib.pylab as plt
+#from scipy.linalg import inv
+from laplaciana import laplaciana
+N=[2,5,10,20,40,50,70,100,400,800,1500,2000,2500,5000,6000]
+UsoMemoria=[]
+UsoTiempo=[]
+Nlista=[]
+for a in range(10):
+    for i in N:
+        t1=perf_counter()
+        A=laplaciana(i, half)
+        t2=perf_counter()
+        Am1=inv(A)
+        t3=perf_counter()
+        t_ens=t2-t1
+        t_inv=t3-t2
+        UsoTiempo.append(t_inv)
+        memoria=  A.nbytes+Am1.nbytes
+        UsoMemoria.append(memoria)
+        Nlista.append(i)
+print(A.nbytes, Am1.nbytes)
+
+
+#Creacion del archivo para guardar datos
+archivo= open("recursos_inv_caso_1_half.txt", "w+")
+for i in range(len(N*10)):
+    archivo.write(str(UsoMemoria[i])+" "+str(UsoTiempo[i])+" "+str(Nlista[i])+" "+"\n")
+archivo.close()
+
---
+++ P03/Codigos/timing_inv_caso_3_single.py
@@ -0,0 +1,31 @@
+from scipy import single
+from time import perf_counter
+import matplotlib.pylab as plt
+from scipy.linalg import inv
+from laplaciana import laplaciana
+N=[2,5,10,20,40,50,70,100,400,800,1500,2000,2500,5000,6000]
+UsoMemoria=[]
+UsoTiempo=[]
+Nlista=[]
+for a in range(10):
+    for i in N:
+        t1=perf_counter()
+        A=laplaciana(i, single)
+        t2=perf_counter()
+        Am1=inv(A, overwrite_a=True)
+        t3=perf_counter()
+        t_ens=t2-t1
+        t_inv=t3-t2
+        UsoTiempo.append(t_inv)
+        memoria=  A.nbytes+Am1.nbytes
+        UsoMemoria.append(memoria)
+        Nlista.append(i)
+print(A.nbytes, Am1.nbytes)
+
+
+
+#Creacion del archivo para guardar datos
+archivo= open("recursos_inv_caso_3_single.txt", "w+")
+for i in range(len(N*10)):
+    archivo.write(str(UsoMemoria[i])+" "+str(UsoTiempo[i])+" "+str(Nlista[i])+" "+"\n")
+archivo.close()
